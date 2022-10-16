4a39bf2fff8bd5165344ee0c5188569bb5ff350d
jitesh-rajput <jiturajput025@gmail.com>
2022-10-16 16:47:27 +0530

    Amplify Authentication With Nuxt2

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "amplifyauthc9ffbfc0": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
